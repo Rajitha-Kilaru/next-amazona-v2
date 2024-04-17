8496c38505ebcb8e0be79dfab93d80a85de26a5d
rajitha.chowdary <rajitha.chowdary@cognitivzen.com>
2024-04-17 15:47:08 +0530

    01-Create-Next-app-layout

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next Amazona V2",
+  description: "Modern Ecommerce Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+        {children}
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>{`Copyright @ ${new Date().getFullYear()} - All right reserved by Next Amazon V2`}</p>
+        </footer>
+        </div>
+        </body>
+    </html>
+  );
+}
